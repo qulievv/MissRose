9d60277a5a820ba38044e574ed70e5553be06203
Revan <101807432+qulievv@users.noreply.github.com>
2022-07-12 18:42:56 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-class Config:
-    BOT_TOKEN = "" # 🌚 get it from @botfather (telegram) by sending /newbot command.
-    API_ID =  # 🌚 Same as APP_ID. Get it from my.telegram.org.
-    API_HASH = "" # 🌚 Get it from my.telegram.org.
-    
+import os
+
+
+
+API_ID = os.getenv('API_ID',12345)                            # You can get this from -->  https://my.telegram.org/auth  
+API_HASH = os.getenv('API_HASH','You API HASH')               # You can get this from -->  https://my.telegram.org/auth
+BOT_TOKEN = os.getenv('BOT_TOKEN','Bot token here')           # You can get this from --> @BotFather
+BOT_NAME = os.getenv('BOT_NAME','RavenSongRobot')           #Username of your bot

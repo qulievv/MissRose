e87b03eb3fe53653bc811784028d0e6e138239c9
Revan <101807432+qulievv@users.noreply.github.com>
2022-07-12 18:46:42 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-class config:
-
-
-
+class Config:
 API_ID = os.getenv('API_ID',12345)                            # You can get this from -->  https://my.telegram.org/auth  
 API_HASH = os.getenv('API_HASH','You API HASH')               # You can get this from -->  https://my.telegram.org/auth
 BOT_TOKEN = os.getenv('BOT_TOKEN','Bot token here')           # You can get this from --> @BotFather
